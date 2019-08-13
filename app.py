e5fb389d7c4022baea6938502f212cbd6d6da8f8
Ilya Osipov <070809010@mail.ru>
2019-08-13 19:39:59 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import json
+from datetime import datetime
+
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    """
+    При верификации вебхука у Facebook он отправит запрос на этот адрес. На него нужно ответить VERIFY_TOKEN.
+    """
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+    """
+    Основной вебхук, на который будут приходить сообщения от Facebook.
+    """
+    data = request.get_json()
+    if data["object"] == "page":
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+                if messaging_event.get("message"):  # someone sent us a message
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+                    send_message(sender_id, message_text)
+    return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+    params = {"access_token": os.environ["PAGE_ACCESS_TOKEN"]}
+    headers = {"Content-Type": "application/json"}
+    request_content = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    response = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=request_content)
+    response.raise_for_status()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Facebook bot webhook
+
+This is a simple Facebook Messenger Webhook, which allows you to receive and send messenges.
+
+1. Use tutorials to get API key:
+    - [русский]()
+    - [english]()
+2. Create [heroku](heroku.com) account and create app there.
+3. Go to `Settings` section and add `Config Vars` pair: `PAGE_ACCESS_TOKEN` and token you got via first step.
+4. Add another pair: `VERIFY_TOKEN` and any value you want.
+5. Go to `Deploy` section and deploy that bot
+
+Now click on `open app` button and you would get "Hello world" page.
+
+If you not, use [heroku logs](https://devcenter.heroku.com/articles/logging#view-logs) to find bug.
